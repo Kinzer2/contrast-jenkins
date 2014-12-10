343c7b4ede9cedd3824079c8a370518a1a6e130c
Chris Schmidt <chris.schmidt@contrastsecurity.com>
2014-12-10 10:08:26 -0700

    Working on code cleanup and application drop-down selection

---
+++ src/main/java/com/contrast/jenkins/contrastjenkins/ContrastJenkinsDescriptor.java
@@ -0,0 +1,7 @@
+package com.contrast.jenkins.contrastjenkins;
+
+/**
+ * Created by cschmidt on 12/10/14.
+ */
+public class ContrastJenkinsDescriptor {
+}
---
+++ src/main/java/com/contrast/jenkins/contrastjenkins/util/ContrastConnectionFactory.java
@@ -0,0 +1,7 @@
+package com.contrast.jenkins.contrastjenkins.util;
+
+/**
+ * Created by cschmidt on 12/10/14.
+ */
+public class ContrastConnectionFactory {
+}
